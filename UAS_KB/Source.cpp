e4ca153aeb8940ced2a264b32e78c777818d7739
madayuki <madayuki@MADAYUKI>
2021-06-10 21:53:36 +0800

    line 8

--- UAS_KB/Source.cpp
+++ UAS_KB/Source.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	std::cout << "Branch ARF" << std::endl;
 	std::cout << "Abed goblok" << std::endl;
+	std::cout << "vincent asyu" << std::endl;
 
 	UAS_KB::Game game(SCREEN_WIDTH, SCREEN_HEIGHT, "Hex Game Minimax");
 	return EXIT_SUCCESS;
