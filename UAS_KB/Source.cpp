45632df372cb46def93ef1c59cffa908d8479574
Kainen Madison <kainenmi8@gmail.com>
2021-06-10 20:38:27 +0700

    di line 6 ku edit

--- UAS_KB/Source.cpp
+++ UAS_KB/Source.cpp
@@ -3,6 +3,9 @@
 int main()
 {
 	std::cout << "Branch ARF" << std::endl;
+	std::cout << "Abed goblok" << std::endl;
+
 	UAS_KB::Game game(SCREEN_WIDTH, SCREEN_HEIGHT, "Hex Game Minimax");
 	return EXIT_SUCCESS;
+
 }
