e81a140e7741f8f666a9b001ad2d64d8f3f79eb9
Vincent Darmawan <vincentd12346@gmail.com>
2021-06-10 21:30:44 +0700

    tambah kucing

--- UAS_KB/Source.cpp
+++ UAS_KB/Source.cpp
@@ -5,6 +5,7 @@ int main()
 	std::cout << "Branch ARF" << std::endl;
 	std::cout << "Abed goblok" << std::endl;
 	std::cout << "vincent asyu" << std::endl;
+	std::cout << "Michael lie meong meong" << std::endl;
 
 	UAS_KB::Game game(SCREEN_WIDTH, SCREEN_HEIGHT, "Hex Game Minimax");
 	return EXIT_SUCCESS;
